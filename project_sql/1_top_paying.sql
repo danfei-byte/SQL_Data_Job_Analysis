3f72145a1a42fed7952febee22f574dc70d3afeb
danfei-byte <danfeili@umd.edu>
2025-01-14 16:56:12 -0500

    t

--- project_sql/1_top_paying.sql
+++ project_sql/1_top_paying.sql
@@ -23,20 +23,3 @@ WHERE
 ORDER BY salary_year_avg DESC
 LIMIT 10;
 
--- Non remote data analyst job market
-SELECT job_title, 
-        job_title_short, 
-        job_location, 
-        job_posted_date :: DATE AS posted_date, 
-        salary_year_avg, 
-        company_dim.name AS company_name
-FROM job_postings_fact
-LEFT JOIN company_dim
-ON job_postings_fact.company_id = company_dim.company_id
-WHERE 
-    job_location != 'Anywhere'
-    AND job_title_short = 'Data Analyst'
-    AND salary_year_avg IS NOT NULL
-ORDER BY salary_year_avg DESC
-LIMIT 10;
-
